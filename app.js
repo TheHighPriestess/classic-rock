0b6d5690960c256596f79414e9143ddb072b417c
TheHighPriestess <elizabeth.hatleli@gmail.com>
2015-01-01 22:27:35 +0200

    My Commit Message

--- app.js
+++ app.js
@@ -48,9 +48,6 @@ $(document).ready(function() {
     
     $("#question_wrapper").on("click", "#retry_button", function () {
         numberCorrect = 0;
-        currentQuestion = 0;
-        $(".score_cup").css("display", "none");
-        $("#score_cup0").css("display", "inline");
         var newQuestion = '<span class="question">'+questions[currentQuestion].question+'</span><br><div id="answer_holder"><input type="radio" name="option" class="option" value="0"><span class="answer">'+questions[currentQuestion].choices[0]+'</span><br><input type="radio" name="option" class="option" value="1"><span class="answer">'+questions[currentQuestion].choices[1]+'</span><br><input type="radio" name="option" class="option" value="2"><span class="answer">'+questions[currentQuestion].choices[2]+'</span><br><input type="radio" name="option" class="option" value="3"><span class="answer">'+questions[currentQuestion].choices[3]+'</span><br></div><div id="button_holder"><input type="button" id="submit" value="submit"><span id="hint"></span><input type="button" id="retry_button" value="again!"></div>';
         $("#question_wrapper").html(newQuestion);
         $("#last_question_fact").html("");
@@ -88,12 +85,13 @@ $(document).ready(function() {
             $("#retry_button").css("display", "inline"); /* displays the 'play again' button */
             var lastFact= questions[currentQuestion-1].fact;  /* creates a variable 'lastfact' and sets it to the fact of the previous question*/
             $("#last_question_fact").html(lastFact); /* displays that fact */
-            if (numberCorrect > 3) {
-                var finalScore = '<span id="final">Congratulations on finishing the quiz!  You correctly answered '+numberCorrect+' question.'
+
+            if (numberCorrect >= 3) {
+                var finalScore = '<span id="final">You correctly answered '+numberCorrect+' questions. Not bad! Are you a groupie by any chance?'
                 $("#answer_holder").html(finalScore);
             }
             else {
-                var finalScore = '<span id="final">Hmmmm.  You correctly answered '+numberCorrect+' questions.'
+                var finalScore = '<span id="final">Hmmmm.  You correctly answered only '+numberCorrect+' of the questions. Maybe you should try the Justin Bieber quiz instead.'
                 $("#answer_holder").html(finalScore);
             }
         }
